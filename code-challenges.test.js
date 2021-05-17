2bae58db5c2411655c5fdac582f7af08b8280d1f
JJSanch <jon.sanchez003@gmail.com>
2021-05-17 08:39:28 -0700

    week 2 assessment initial commit

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,4 +1,10 @@
+// NOTE:  I rewatched some videos and attempted to get through the challanges but was ultimately not successful.  If possible, I would like to redo this challenge
+
+
 // ASSESSMENT 2: Coding practical questions with Jest
+
+const { test, expect } = require("@jest/globals")
+const { describe } = require("yargs")
 
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
@@ -15,8 +21,21 @@
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
+// Create a function to decide if a number is divisible by 3
+// use describe and nested functions to create the function
+// write out the name of the expected function
+ describe("divBy3", () => {
+ 
 // a) Create a test with expect statements for each of the variables provided.
-
+// write out a description of the test
+    test ("returns 'number is divisible by 3'", () => {
+        //write expected output for variables depending if they are divisible by 3
+        expect(divBy3("15", "3")).toEqual("number is divisible by 3")
+        expect(divBy3("-7")).toEqual("is not divisible by three")
+        })
+ })
+// include variable to input and test for the expected output
+//use .toEqual as a matcher function
 var num1 = 15
 // Expected output: "15 is divisible by three"
 
@@ -24,19 +43,37 @@ var num2 = 0
 // Expected output: "0 is divisible by three"
 
 var num3 = -7
-// Expected output: "-7 is not divisble by three"
+// Expected output: "-7 is not divisible by three"
+
 
 
 
 
 
 // b) Create the function that makes the test pass.
+//write out function
+const divBy3 = (string) => {
+    // if statement for numbers that can be divided by 3
+    if(string === "number is divisible by 3"){
+        return "is divisible by 3"
+    //  // e;se if statement for numbers that cannot be divided by 3
+    } else if(string === "number is not divisble by 3") {
+        return "not divisible by 3"
+    }
+}
+// write out the function with the expected output
+const divBy3 = () => {
+    return "is divisible by 3"
+}
 
 
 
 
 
-// --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+// --------------------2)
+describe("allCaps", () => {
+
+}
 
 // a) Create a test with expect statements for each of the variables provided.
 
--- interview-questions.md
+++ interview-questions.md
@@ -8,17 +8,17 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. The built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: n/a
 
-  Researched answer:
+  Researched answer: The predefined parameters are  value, index and array.  Value is required but and index and array are optional
 
 
 
 2. What is object destructuring?
 
-  Your answer:
+  Your answer: Taking an object and placing them into different variables
 
-  Researched answer:
+  Researched answer: Destructuring is taking the properties of an object and placing them into individual, receallable variables.
 
 
 
@@ -32,17 +32,17 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 4. What is a React Fragment? Why would you use one?
 
-  Your answer:
+  Your answer: n/a
 
-  Researched answer:
+  Researched answer: Allows one to wrap or group muliple elements without adding an extra node to the DOM
 
 
 
 5. What is JSX?
 
-  Your answer:
+  Your answer: JSX is a syntax used to combine html and JS for use with react
 
-  Researched answer:
+  Researched answer: JSX is a syntax extension to JS.  Behaves like html with a few exceptions.
 
 
 
@@ -58,8 +58,8 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. React props:
-2. React lifecycle methods:
-3. Logic/smart component:
+1. React props: "Props" stands for properties.  Used for passing information from one component to another
+2. React lifecycle methods: The lifecycle of a a react component is mounting, upodating and unmounting.
+3. Logic/smart component: Smart components call out to things outside of themselves.
 4. Display/dumb component:
 5. DOM Events:
